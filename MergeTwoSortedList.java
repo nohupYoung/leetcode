f98db36671813d44f3f6979cc21abb829e19d6df
nohupYoung <yangbingjiaofree@gmail.com>
2016-04-30 15:44:31 +0800

    MergeTwoSortedLists.java

--- MergeTwoSortedList.java
+++ MergeTwoSortedList.java
@@ -1,5 +1,33 @@
-public class MergeTwoSortedList{
-	public ListNode mergeTwoSortedList(ListNode p,ListNode q) {
-		
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class MergeTwoSortedList {
+	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+		ListNode dummy = new ListNode(0);
+        	ListNode lastNode = dummy;
+        
+        	while (l1 != null && l2 != null) {
+            		if (l1.val < l2.val) {
+                		lastNode.next = l1;
+                		l1 = l1.next;
+            		} else {
+                		lastNode.next = l2;
+                		l2 = l2.next;
+            		}
+            		lastNode = lastNode.next;
+        	}
+        
+        	if (l1 != null) {
+            		lastNode.next = l1;
+        	} else {
+           		lastNode.next = l2;
+        	}
+        
+        	return dummy.next;		        
 	}
 }
