02df0956f4347d71e19483368152c18038f9eaf2
nohupYoung <yangbingjiaofree@gmail.com>
2016-05-02 23:10:17 +0800

    SwapNodesInPairs.java

--- SwapNodesInPairs.java
+++ SwapNodesInPairs.java
@@ -6,9 +6,22 @@
  *     ListNode(int x) { val = x; }
  * }
  */
-public class SwapNodesinPairs {
-	public ListNode swapPairs(ListNode head) {
-		
+public class SwapNodesInPairs {
+    public ListNode swapPairs(ListNode head) {
+    	ListNode tmp = new ListNode(0);
+    	tmp.next=head;
+    	head=tmp;
+    	while (head.next!=null && head.next.next!=null){
+    		ListNode p1=head.next;
+    		ListNode p2=head.next.next;
+
+    		head.next=p2;
+    		p1.next=p2.next;
+    		p2.next=p1;
+    		//move to next pair ListNodes
+    		head=p1;
+    	}
+    	return tmp.next;
         
-	}
+    }
 }
