3c2097c019e05b680c89573a132815c8738687b1
nohupYoung <yangbingjiaofree@gmail.com>
2016-05-09 10:06:28 +0800

    PalindromeNumber.java

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -1,5 +1,21 @@
 public class PalindromeNumber {
 	public boolean isPalindrome(int x) {
-        
+		if (x<0) return false;
+		
+		int count = 1;
+		while (x/count >= 10) {
+			count *= 10;
+		}
+		while (x != 0) {
+			int left = x/count;
+			int right = x%10;
+ 
+			if (left!=right)
+				return false;
+			//将头尾去掉
+			x = (x%count)/10;
+			count /= 100;
+		}
+		return true;
 	}
 }
