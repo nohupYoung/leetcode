d9d2c4c4d1b5eddffa8a82fa989e25fdd81b2afd
nohupYoung <yangbingjiaofree@gmail.com>
2016-05-11 21:45:03 +0800

    RemoveDuplicatesFromSortedArray.java

--- RemoveDuplicatesFromSortedArray.java
+++ RemoveDuplicatesFromSortedArray.java
@@ -1,6 +1,20 @@
 public class RemoveDuplicatesFromSortedArray {
 	public int removeDuplicates(int[] nums) {
-		if(nums==null || nums.length==1) return nums;
+		if(nums.length==0) return 0;
+		if(nums.length==1) return 1;
+		int fast=1;
+		int slow=1;
+		int current=nums[0];
+		while(fast<nums.length) {
+			if(nums[fast]!=current) {
+				nums[slow]=nums[fast];
+				current=nums[slow];
+				slow++;
+			} else {
+				fast++;
+			}
+		}
+		return slow;
         
 	}
 }
