2fbd0860c64d9cbb45ac7cc75f7d16b88def36c0
nohupYoung <yangbingjiaofree@gmail.com>
2016-05-12 22:10:30 +0800

    PascalTriangle.java

--- PascalTriangle.java
+++ PascalTriangle.java
@@ -1,5 +1,24 @@
 public class PascalTriangle {
-	public List<List<Integer>> generate(int numRows) {
-        
-	}
+    public List<List<Integer>> generate(int numRows) {
+		List<List<Integer>> list = new ArrayList<List<Integer>>();
+		if(numRows==0) return list;
+		ArrayList<Integer> first=new ArrayList<>();
+		first.add(1);
+		list.add(first);
+		for(int i=1;i<numRows;i++) {
+			ArrayList<Integer> current = new ArrayList<>();
+			current.add(1);
+			List<Integer> previous=list.get(i-1);
+			for(int j=1;j<i;j++) {
+				current.add(previous.get(j-1)+previous.get(j));
+			}
+			current.add(1);
+			list.add(current);
+			
+		}
+		
+        return list;
+    }
+
+
 }
