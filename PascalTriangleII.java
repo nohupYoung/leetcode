720c3b521e54d7c3c636122b5bc40d68a1454c06
nohupYoung <yangbingjiaofree@gmail.com>
2016-05-16 23:20:09 +0800

    PascalTriangleII.java

--- PascalTriangleII.java
+++ PascalTriangleII.java
@@ -3,11 +3,23 @@ import java.util.List;
 
 public class PascalTriangleII {
 	public List<Integer> getRow(int rowIndex) {
-		List<Integer> list=new ArrayList<>();
-		
+        List<Integer> list=new ArrayList<>();
+		if(rowIndex<0) return null;
+		list.add(1);
+		for(int i=1;i<=rowIndex;i++) {
+			List<Integer> current = new ArrayList<>(i+1);
+			for (int j = 0; j <= i; j++) {
+				current.add(0);
+            }
+			current.set(0, 1);
+			current.set(i, list.get(i-1));
+			for(int j=1;j<i;j++) {
+				current.set(j, list.get(j)+list.get(j-1));
+			}
+			list=current;
+		}
 		
 		return list;
         
-	}
-
+    }
 }
