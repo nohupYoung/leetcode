de695ece44b2f1eb0a7485246981267895ff123c
nohupYoung <yangbingjiaofree@gmail.com>
2016-05-20 23:06:27 +0800

    FactorialTrailingZeroes.java

--- FactorialTrailingZeroes.java
+++ FactorialTrailingZeroes.java
@@ -1,6 +1,11 @@
 public class FactorialTrailingZeroes {
 	public int trailingZeroes(int n) {
-		
-        
+		if(n<1) return 0;
+		int res=0;
+		while(n/5 !=0){
+			n/=5;
+			res+=n;
+		}
+		return res;
 	}
 }
