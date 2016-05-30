9407d0d97ca5bf4bab42db4d3561d1f7d428a4ba
nohupYoung <yangbingjiaofree@gmail.com>
2016-05-30 10:36:04 +0800

    ContainsDuplicateII.java

--- ContainsDuplicateII.java
+++ ContainsDuplicateII.java
@@ -1,5 +1,15 @@
 public class ContainsDuplicateII {
 	public boolean containsNearbyDuplicate(int[] nums, int k) {
-        
+		if(nums==null || nums.length<2) return false;
+					
+		Map<Integer, Integer> map = new HashMap<>();
+		for(int i=0;i<nums.length;i++){
+			if(map.containsKey(nums[i])){
+				int j = map.get(nums[i]);
+				if(i-j<=k) return true;
+			} 
+			map.put(nums[i], i);
+		}		
+		return false;		
 	}
 }
