ab27236ca15f2ee950a3e59bb4d129bcba70e485
nohupYoung <yangbingjiaofree@gmail.com>
2016-06-01 00:49:15 +0800

    RectangleArea.java

--- RectangleArea.java
+++ RectangleArea.java
@@ -1,7 +1,7 @@
 public class RectangleArea {
 	public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
 		int sum=(C - A) * (D - B) + (H - F) * (G - E);
-		
-        
+		if (E >= C || F >= D || B >= H || A >= G) return sum;
+        	return sum - ((Math.min(G, C) - Math.max(A, E)) * (Math.min(D, H) - Math.max(B, F)));
 	}
 }
