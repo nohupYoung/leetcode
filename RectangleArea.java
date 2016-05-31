55d7dbe6b14eb9e20f02949d8efdb457d3df52d7
nohupYoung <yangbingjiaofree@gmail.com>
2016-05-31 23:15:31 +0800

    RectangleArea.java

---
+++ RectangleArea.java
@@ -0,0 +1,7 @@
+public class RectangleArea {
+	public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+		int sum=(C - A) * (D - B) + (H - F) * (G - E);
+		
+        
+	}
+}
