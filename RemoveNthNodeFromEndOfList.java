aeda18b0ced4dd44adbd6792da28c8656a3ba963
nohupYoung <yangbingjiaofree@gmail.com>
2016-06-09 23:03:31 +0800

    RemoveNthNodeFromEndOfList.java

--- RemoveNthNodeFromEndOfList.java
+++ RemoveNthNodeFromEndOfList.java
@@ -8,7 +8,22 @@
  */
 public class RemoveNthNodeFromEndOfList {
 	public ListNode removeNthFromEnd(ListNode head, int n) {
-
+		if(head==null || head.next==null) return null;
+		ListNode fast=head;
+		ListNode slow=head;
+		for(int i=0;i<n;i++) {
+			fast=fast.next;
+		}
+		if(fast==null) {
+			head=head.next;
+			return head;
+		}
+		while(fast.next != null) {
+			slow=slow.next;
+			fast=fast.next;
+		}
+		slow.next=slow.next.next;
+		return head;
         
 	}
 }
