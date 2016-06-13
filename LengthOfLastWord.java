9016e7d38f5f1441202fd3ce060ebc6b14e6d8fa
nohupYoung <yangbingjiaofree@gmail.com>
2016-06-13 23:36:14 +0800

    LengthOfLastWord.java

--- LengthOfLastWord.java
+++ LengthOfLastWord.java
@@ -1,6 +1,10 @@
 public class LengthOfLastWord {
 	public int lengthOfLastWord(String s) {
-		
-        
+		if (s != null && !s.trim().equals("")) {
+            		String[] arr = s.trim().split(" ");
+            		int length = arr[arr.length - 1].length();
+            		return length;
+      		}
+      		return 0;
 	}
 }
