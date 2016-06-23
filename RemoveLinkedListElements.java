a3a75a0c3ef2e1ad7c6c8e164c1fc1c22de32d7c
nohupYoung <yangbingjiaofree@gmail.com>
2016-06-23 23:05:28 +0800

    RemoveLinkedListElements.java

--- RemoveLinkedListElements.java
+++ RemoveLinkedListElements.java
@@ -8,6 +8,17 @@
  */
 public class RemoveLinkedListElements {
 	public ListNode removeElements(ListNode head, int val) {
+		ListNode tmp=new ListNode(0);
+		tmp.next=head;
+		head=tmp;
+		while(head.next != null) {
+			if(head.next.val==val) {
+				head.next=head.next.next;
+			} else {
+				head=head.next;
+			}
+		}
+		return tmp.next;
         
 	}
 }
