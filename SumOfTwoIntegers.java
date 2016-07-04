266125c3f72c6f2536c74187ace36075ffa02c7c
nohupYoung <yangbingjiaofree@gmail.com>
2016-07-04 22:42:26 +0800

    SumOfTwoIntegers.java

--- SumOfTwoIntegers.java
+++ SumOfTwoIntegers.java
@@ -1,6 +1,10 @@
-public class SumOfTwoIntegers {	
-	public int getSum(int a, int b) {
-		
-        
-	}
+public class SumOfTwoIntegers {
+    public int getSum(int a, int b) {
+        while (b != 0) {
+            int c = a ^ b;
+            b = (a & b) << 1;
+            a = c;
+        }
+        return a;
+    }
 }
